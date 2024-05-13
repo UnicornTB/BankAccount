9d6bf5334c6d21f593bf505e69cd675221633fe9
Dolgy Sergey <vtb4102285@region.vtb.ru>
2024-05-13 16:29:36 +0300

    init

---
+++ src/test/java/org/example/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
